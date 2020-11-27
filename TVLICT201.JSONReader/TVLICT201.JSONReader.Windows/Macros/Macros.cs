ae6404794f80715a755e2e9bcf2c799886f1a995
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-11-27 10:17:53 +0400

    reading json

---
+++ TVLICT201.JSONReader/TVLICT201.JSONReader.Windows/Macros/Macros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVLICT201.JSONReader.Windows.Macros
+{
+     public class Macros
+    {
+        public string Id { get; set; }
+        public string Name{ get; set; }
+        public string Calories { get; set; }
+        public string Fat { get; set; }
+        public string Carbohydrates { get; set; }
+        public string Protein { get; set; }
+
+    }
+}
---
+++ TVLICT201.JSONReader/TVLICT201.JSONReader.Windows/Macros/MacrosWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TVLICT201.JSONReader.Windows.Macros
+{
+    /// <summary>
+    /// Interaction logic for MacrosWindow.xaml
+    /// </summary>
+    public partial class MacrosWindow : Window
+    {
+        public MacrosWindow()
+        {
+            InitializeComponent();
+            Macros macros = new Macros();
+
+            using (StreamReader r = new StreamReader("data\\macros.json"))
+            {
+                string json = r.ReadToEnd();
+                macros = JsonConvert.DeserializeObject<Macros>(json);
+            }
+
+            lblMacrosId.Content = "id :" + macros.Id;
+            lblMacrosName.Content = "name :" + macros.Name;
+            lblMacrosCalories.Content = "calories :" + macros.Calories;
+            lblMacrosFat.Content = "fat :" + macros.Fat;
+            lblMacrosCarbohydrates.Content = "carbohydrates :" + macros.Carbohydrates;
+            lblMacrosProtein.Content = "protein :" + macros.Protein;
+
+        }
+    }
+}
